6e2a3d2ed8dab71deac82e2eebc822933b27526e
danne128 <daniel.trondsen.wallin@yh.nackademin.se>
2016-08-30 13:09:25 +0200

    Initial Commit

---
+++ Test projekt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test projekt
+//
+//  Created by Daniel Trondsen Wallin on 30/08/16.
+//  Copyright © 2016 Daniel Trondsen Wallin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
